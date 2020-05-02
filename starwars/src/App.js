e1fc4630c0519d14dad14060fa9e417905357cab
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-05-01 22:06:41 -0400

    chrarater component done just need to figure out async issue

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import Character from "./components/Character";
+
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  const [rmData, setRmData] = useState();
+  useEffect(() => {
+    // Get single character read docs ... updated chose character 15
+    Axios.get("https://rickandmortyapi.com/api/character/15")
+      .then((response) => {
+        console.log("rick and morty data response", response);
+        setRmData(response.data);
+      })
+      .catch((error) => {
+        console.log("rick and morty data error", error);
+      });
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character rmData={rmData} />
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
+// imports
+import React from "react";
+
 // Write your Character component here
+
+const Character = (props) => {
+  console.log("Props from the apps.js file ", props);
+  return (
+    <div>
+      {}
+      <h1>{/*props.rmData.name*/}</h1>
+      <img src={/*props.rmData.image*/} />
+    </div>
+  );
+};
+
+//export file to get it to the APP.js file
+export default Character;
